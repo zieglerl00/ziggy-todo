6128f772b8eec56faa103e6cc29eb9387ef9be53
Lukas Ziegler <94352578+zieglerl00@users.noreply.github.com>
2022-06-24 23:28:40 +0200

    PATCH: DEBUG FALSE

--- traxello/settings.py
+++ traxello/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2d-*j8w=*9d3e@zavf+ep&#1l+yldzp@rgx2*gmt5d&1c44pg#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
