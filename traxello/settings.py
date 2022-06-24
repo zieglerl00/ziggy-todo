015f88b7a0f45aaca3891bae1c43794a22fe7642
Lukas Ziegler <94352578+zieglerl00@users.noreply.github.com>
2022-06-24 23:55:31 +0200

    PATCH: Settings

--- traxello/settings.py
+++ traxello/settings.py
@@ -119,10 +119,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
