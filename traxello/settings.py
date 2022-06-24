45af3e67d64871ee731c0e24edd6e1a4e9e1415f
Lukas Ziegler <94352578+zieglerl00@users.noreply.github.com>
2022-06-25 00:34:28 +0200

    PATCH: fixes

--- traxello/settings.py
+++ traxello/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-2d-*j8w=*9d3e@zavf+ep&#1l+yldzp@rgx2*gmt5d&1c44pg#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "tr4xello.herokuapp.com"]
+ALLOWED_HOSTS = ["*", "tr4xello.herokuapp.com"]
 
 # Application definition
 
@@ -133,6 +135,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
